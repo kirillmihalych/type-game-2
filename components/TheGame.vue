78e7e23e4d6894c05b90369d08dc14794b11ad26
kirillmihalych <tiltlessman@gmail.com>
2025-03-17 20:16:38 +0300

    feat: add an svg bg to the game

--- components/TheGame.vue
+++ components/TheGame.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="h-full border-[1px] border-t-0 ml-4 mr-4 p-4 border-gray-400/35">
-    Game
-  </div>
+  <div
+    class="svg-bg h-full border-[1px] border-t-0 p-4 border-gray-400/35"
+  ></div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style scoped>
+.svg-bg {
+  background-color: #ffffff;
+  background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23000000' fill-opacity='0.05'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+}
+</style>
