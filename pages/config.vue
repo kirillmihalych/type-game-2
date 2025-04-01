da4cd71ed5006a3e3678b05a4c07e41c54955b62
kirillmihalych <tiltlessman@gmail.com>
2025-04-01 22:58:40 +0300

    feat: config responsive design basics

--- pages/config.vue
+++ pages/config.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="p-4">
-    <h2 class="text-2xl text-gray-950">Настройки</h2>
-    <section>
-      <h3 class="text-lg">Типография</h3>
-      <form>
-        <p class="text-gray-950/50">Установить размер шрифта в игре</p>
-        <InputUi
-          type="number"
-          :value="settings.fontSize"
-          @input="settings.setFontSize"
-        />
-      </form>
-    </section>
+    <div>
+      <h1 class="text-2xl text-gray-950 mb-6">Настройки</h1>
+      <section>
+        <div class="flex items-center gap-2 mb-4">
+          <!-- <Icon name="lucide:chevron-right" size="1.5rem" /> -->
+          <Icon name="lucide:chevron-down" size="1.5rem" />
+          <h2 class="text-lg">Типография</h2>
+        </div>
+        <form class="grid lg:grid-cols-2 lg:items-center mb-4" @submit.prevent>
+          <div>
+            <div class="flex items-center gap-2">
+              <Icon name="lucide:letter-text" size="1.5rem" />
+              <h3>Font size</h3>
+            </div>
+            <p class="text-gray-950/50">Установить размер шрифта в игре</p>
+          </div>
+          <InputUi
+            type="number"
+            :value="settings.fontSize"
+            @input="settings.setFontSize"
+          />
+        </form>
+      </section>
+    </div>
   </div>
 </template>
 
