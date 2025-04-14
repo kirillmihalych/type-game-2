04dfc4ae67d4516978a8026cda547441ffb9b6ca
kirillmihalych <tiltlessman@gmail.com>
2025-04-14 12:13:40 +0300

    refactor: reusable config item

---
+++ components/ConfigItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h2 class="hover:text-purple-500 transition-colors text-lg">
+      <button @click="toggle" class="flex items-center gap-2 mb-4">
+        <Icon
+          name="lucide:chevron-right"
+          size="1.5rem"
+          class="transition-transform"
+          :class="[showSection ? 'rotate-90' : 'rotate-0']"
+        />
+        <slot name="header" />
+      </button>
+    </h2>
+    <div v-show="showSection">
+      <slot name="main" />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+const showSection = ref(true);
+
+function toggle() {
+  showSection.value = !showSection.value;
+}
+</script>
