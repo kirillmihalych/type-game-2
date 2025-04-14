1258d07a95a5b03b268f6f4f4d6abbe5039113e4
kirillmihalych <tiltlessman@gmail.com>
2025-04-14 12:13:26 +0300

    refactor: reusable config form

---
+++ components/ConfigForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <form class="grid lg:grid-cols-2 lg:items-center mb-4" @submit.prevent>
+      <div>
+        <div class="flex items-center gap-2">
+          <slot name="header-icon" />
+          <h3>
+            <slot name="heading" />
+          </h3>
+        </div>
+        <p class="text-gray-950/50">
+          <slot name="description" />
+        </p>
+      </div>
+      <div>
+        <slot name="form-input" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
