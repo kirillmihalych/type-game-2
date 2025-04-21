640a4e97eec30fd0b51f88f638bd36d13d2c9664
kirillmihalych <tiltlessman@gmail.com>
2025-04-21 14:01:50 +0300

    feat: style nav links

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,21 +1,30 @@
 <template>
   <nav class="flex justify-between items-center py-4 lg:py-6">
-    <ul class="flex gap-2">
+    <ul class="flex gap-2 text-zinc-400 transition-colors">
       <li>
-        <NuxtLink to="/" class="flex items-center gap-1">
+        <NuxtLink
+          to="/"
+          class="flex items-center gap-1 hover:text-zinc-800 dark:hover:text-zinc-200 focus:outline-2 focus:outline-offset-2 focus:text-zinc-800 active:text-zinc-800dark:active:text-zinc-200 dark:focus:text-zinc-200"
+        >
           <Icon name="heroicons:play" />
           <span> Игра </span>
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/config" class="flex items-center gap-1">
+        <NuxtLink
+          to="/config"
+          class="flex items-center gap-1 hover:text-zinc-800 dark:hover:text-zinc-200 focus:outline-2 focus:outline-offset-2 focus:text-zinc-800 active:text-zinc-800 dark:active:text-zinc-200 dark:focus:text-zinc-200"
+        >
           <Icon name="heroicons:cog-6-tooth" />
           <span> Настройки </span>
         </NuxtLink>
       </li>
     </ul>
-    <div class="flex items-center gap-2">
-      <button class="flex items-center" @click="setDarkMode">
+    <div class="flex items-center gap-2 text-zinc-600">
+      <button
+        class="flex items-center hover:text-zinc-800 dark:hover:text-zinc-200 focus:outline-2 focus:outline-offset-2 focus:text-zinc-800 active:text-zinc-800 dark:active:text-zinc-200 dark:focus:text-zinc-200"
+        @click="setDarkMode"
+      >
         <ClientOnly fallback-tag="span">
           <Icon v-if="isLightMode" name="lucide:moon" size="1.5rem" />
           <Icon v-if="isDarkMode" name="lucide:sun-medium" size="1.5rem" />
@@ -23,7 +32,7 @@
       </button>
       <NuxtLink
         to="https://github.com/kirillmihalych"
-        class="flex items-center"
+        class="flex items-center hover:text-zinc-800 dark:hover:text-zinc-200 focus:outline-2 focus:outline-offset-2 focus:text-zinc-800 active:text-zinc-800 dark:active:text-zinc-200 dark:focus:text-zinc-200"
       >
         <Icon name="uil:github" size="1.75rem" />
       </NuxtLink>
