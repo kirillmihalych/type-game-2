df8e7f43b5202d80551c1b4bdd5d5ee86415aaae
kirillmihalych <tiltlessman@gmail.com>
2025-04-25 12:09:18 +0300

    refactor: icon name

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -25,8 +25,7 @@
         class="flex items-center hover:text-zinc-800 dark:hover:text-zinc-200 focus:outline-2 focus:outline-offset-2 focus:text-zinc-800 active:text-zinc-800 dark:active:text-zinc-200 dark:focus:text-zinc-200"
         @click="setDarkMode"
       >
-        <Icon v-show="isLightMode" name="lucide:moon" size="1.5rem" />
-        <Icon v-show="isDarkMode" name="lucide:sun-medium" size="1.5rem" />
+        <Icon :name="iconName" size="1.5rem" />
       </button>
       <NuxtLink
         to="https://github.com/kirillmihalych"
@@ -46,6 +45,10 @@ const isDarkMode = computed(() => {
 });
 const isLightMode = computed(() => {
   return colorMode.preference === 'light';
+});
+
+const iconName = computed(() => {
+  return isDarkMode.value ? 'lucide:sun-medium' : 'lucide:moon';
 });
 
 function setDarkMode() {
